cbe68b48a9999a9c5e71d03e3dcd6e20f0fe3ca2
Angie Kassab <anasab12@gmail.com>
2018-04-22 02:50:29 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,14 +1,4 @@
-board = [
-  first_row  =[ " ", " ", " "]# Define the variable board below.
-  first_row  [0]
-  first_row  [1]
-  first_row  [2]
-  second_row = " ", " ", " "]
-  second_row  [3]
-  second_row  [4]
-  second_row  [5]
-  third_row  =[ " ", " ", " "]
-  third_row [6]
-  third_row [7]
-  third_row  [8]
-]
+board = [ " ", " ", " ",
+         " ", " ", " ",
+          " ", " ", " ",]# Define the variable board below.
+  
